df28c3f4640b8853007771684f3255a87a3e2b65
Meet-1512 <iconic1512@gmail.com>
2026-04-09 19:38:53 +0530

    Update Terms of Use page

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -29,12 +29,11 @@ export default function Terms() {
               Ardira Terms and Conditions
             </h2>
             <p className="mb-4">
-              The website located at 
-              <a
+              The website located at <a
                 href="https://www.ardira.com"
                 className="text-emerald-600 hover:text-emerald-700 underline"
               >
-                www.ardira.com
+              www.ardira.com
               </a>, (the "Website") is
               published, owned, and operated by Ardira Corporation, its
               affiliates and related entities ("Ardira", "the Company," "we,"
